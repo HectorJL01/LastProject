99713a1b5c1f0f334f52fb648f76971f3c93f8cb
hector <hectorjl01@Hectors-MacBook-Pro.local>
2018-07-07 11:24:00 -0400

    pages and components

--- src/pages/Staff/Staff.js
+++ src/pages/Staff/Staff.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Staff.css";
-import "./Staffcard";
-import "./Staffform";
+import Staffcard from "../../components/Staffcard/Staffcard.js";
+import Staffform from "../../components/Staffform/Staffform.js";
 
 
 
 const Staff = props => (
+  <div className="<Staffcard>
+  </div>
+
+
 
 );
 
