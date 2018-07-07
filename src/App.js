e8db9028b11530ac8fd0fdb2a03ce49d60598d65
hector <hectorjl01@Hectors-MacBook-Pro.local>
2018-07-07 12:57:08 -0400

    calendar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import Log from "./components/Log";
 import Staff from "./pages/Staff";
 import Patient from "./pages/Patient"
 import Schedule from "./components/Schedule";
+import Booking from "./pages/Booking";
+
+import Timeline from 'react-calendar-timeline/lib'
+// make sure you include the timeline stylesheet or the timeline will not be styled
+import 'react-calendar-timeline/lib/Timeline.css'
+import moment from 'moment';
 
 
 
@@ -14,11 +20,19 @@ const App = () => (
   <Router>
     <div>
       <Nav />
+      {/* <Timeline
+      groups={groups}
+      items={items}
+      defaultTimeStart={moment().add(-12, 'hour')}
+      defaultTimeEnd={moment().add(12, 'hour')}
+    /> */}
+      
       <Switch>
         <Route exact path="/log" component={Log} />
         <Route exact path="/staff" component={Staff} />
         <Route exact path="/patient" component={Patient} />
         <Route exact path="/schedule" component={Schedule} />
+        <Route exact path="/booking" component={Booking} />
       </Switch>
     </div>
   </Router>
